9d83ce41ca3e665287253616e4c4699ed10a4f32
grikkco-bit <grikkco@gmail.com>
2025-10-20 15:38:05 +0300

    Убрала

--- script.js
+++ script.js
@@ -333,16 +333,7 @@ if (showResultBtn) {
 }
 closeResultBtn.addEventListener('click', closeModal);
 closeBtn.addEventListener('click', closeModal);
-shareResultBtn.addEventListener('click', () => {
-  // try Web Share API or fallback to copying text
-  const res = computeResult();
-  const shareText = `${res.title}: ${res.text}`;
-  if (navigator.share) {
-    navigator.share({ title: res.title, text: res.text }).catch(()=>{});
-  } else {
-    navigator.clipboard?.writeText(shareText).then(()=> alert('Результат скопирован в буфер обмена'));
-  }
-});
+
 
 // build first game on load
 buildGrid();
